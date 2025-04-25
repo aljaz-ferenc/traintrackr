e82ed5f4568bdd3bef848a203639b52c0d3201ce
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2025-04-25 14:20:33 +0200

    refactor mobile sidebar to its own component

---
+++ src/components/Sidebar/MobileSidebar.tsx
@@ -0,0 +1,45 @@
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet.tsx";
+import { Menu } from "lucide-react";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import Sidebar from "@/components/Sidebar/Sidebar.tsx";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+type MobileSidebarProps = {
+	onOpenChange: (open: boolean) => void;
+};
+
+export default function MobileSidebar({ onOpenChange }: MobileSidebarProps) {
+	const [isOpen, setIsOpen] = useState(false);
+	const { t } = useTranslation();
+
+	const handleOpenChange = (open: boolean) => {
+		setIsOpen(open);
+		onOpenChange(isOpen);
+	};
+
+	return (
+		<div className="block lg:hidden">
+			<Sheet open={isOpen} onOpenChange={(open) => handleOpenChange(open)}>
+				<SheetTrigger asChild>
+					<button type="button" className="cursor-pointer">
+						<Menu size={20} />
+					</button>
+				</SheetTrigger>
+				<SheetContent side="left" className="w-fit">
+					<VisuallyHidden>
+						<SheetTitle>{t("SIDEBAR.title")}</SheetTitle>
+						<SheetDescription>{t("SIDEBAR.description")}</SheetDescription>
+					</VisuallyHidden>
+					<Sidebar onLinkClick={() => handleOpenChange(false)} />
+				</SheetContent>
+			</Sheet>
+		</div>
+	);
+}
--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,14 +1,13 @@
 import { Outlet } from "react-router";
 import Sidebar from "../Sidebar/Sidebar";
 import ModeToggle from "@/components/shared/ModeToggle";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet.tsx";
-import { Menu } from "lucide-react";
 import { useState } from "react";
 import { UserButton } from "@clerk/clerk-react";
 import SelectLanguage from "@/components/shared/SelectLanguage.tsx";
+import MobileSidebar from "@/components/Sidebar/MobileSidebar.tsx";
 
 export default function AppLayout() {
-	const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+	const [_, setSidebarIsOpen] = useState(false);
 
 	return (
 		<div className="flex min-h-screen relative">
@@ -18,18 +17,7 @@ export default function AppLayout() {
 			/>
 			<main className="min-h-screen mx-auto w-full p-3 sm:p-5  lg:p-10 max-w-[1440px]">
 				<div className="flex items-center gap-3">
-					<div className="block lg:hidden">
-						<Sheet open={sidebarIsOpen} onOpenChange={setSidebarIsOpen}>
-							<SheetTrigger asChild>
-								<button type="button" className="cursor-pointer">
-									<Menu size={20} />
-								</button>
-							</SheetTrigger>
-							<SheetContent side="left" className="w-fit">
-								<Sidebar onLinkClick={() => setSidebarIsOpen(false)} />
-							</SheetContent>
-						</Sheet>
-					</div>
+					<MobileSidebar onOpenChange={setSidebarIsOpen} />
 					<div className="flex gap-3 items-center ml-auto">
 						<UserButton />
 						<ModeToggle />
